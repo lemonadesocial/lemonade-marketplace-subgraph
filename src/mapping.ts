55ffa2af7341e24596c79b509f15f8049881d245
Chris Lahaye <mail@chrislahaye.com>
2021-08-06 09:53:17 +0200

    feat: add support for bids and query ERC20 and ERC721 contracts when order is created
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,40 +1,92 @@
 
-import { OfferCreated, OfferWithdrawn, OfferAccepted } from '../generated/LemonadeMarketplace/LemonadeMarketplace';
-import { Offer } from '../generated/schema'
+import { ERC20 } from '../generated/LemonadeMarketplace/ERC20'
+import { ERC721Metadata } from '../generated/LemonadeMarketplace/ERC721Metadata'
+import { OrderCreated, OrderBid, OrderCancelled, OrderFilled } from '../generated/LemonadeMarketplace/LemonadeMarketplace';
+import { Bid, Order } from '../generated/schema'
 
-export function handleOfferCreated(event: OfferCreated): void {
-  let offer = new Offer(event.address.toHex() + '_' + event.params.offerId.toHex());
-  offer.lastBlock = event.block.number;
-  offer.createdAt = event.block.timestamp;
-  offer.offerContract = event.address;
-  offer.offerId = event.params.offerId;
+export function handleOrderCreated(event: OrderCreated): void {
+  let currency = ERC20.bind(event.params.currency);
+  let token = ERC721Metadata.bind(event.params.tokenContract);
 
-  offer.active = true;
-  offer.seller = event.params.seller;
-  offer.currency = event.params.currency;
-  offer.price = event.params.price;
-  offer.tokenContract = event.params.tokenContract;
-  offer.tokenId = event.params.tokenId;
-  offer.save();
+  let order = new Order(event.address.toHex() + '-' + event.params.orderId.toHex());
+  order.lastBlock = event.block.number;
+  order.createdAt = event.block.timestamp;
+  order.orderContract = event.address;
+  order.orderId = event.params.orderId;
+
+  switch (event.params.kind) {
+    case 0:
+      order.kind = 'Direct'; break;
+    case 1:
+      order.kind = 'Auction'; break;
+  }
+
+  order.open = true;
+
+  if (event.params.openFrom) {
+    order.openFrom = event.params.openFrom;
+  }
+
+  if (event.params.openTo) {
+    order.openTo = event.params.openTo;
+  }
+
+  order.maker = event.params.maker;
+  order.currencyContract = currency._address;
+  order.currencyName = currency.name();
+  order.currencySymbol = currency.symbol();
+  order.price = event.params.price;
+  order.tokenContract = token._address;
+  order.tokenId = event.params.tokenId;
+  order.tokenURI = token.tokenURI(event.params.tokenId);
+
+  order.save();
 }
 
-export function handleOfferWithdrawn(event: OfferWithdrawn): void {
-  let offer = new Offer(event.address.toHex() + '_' + event.params.offerId.toHex());
-  if (!offer) return;
+export function handleOrderBid(event: OrderBid): void {
+  let order = new Order(event.address.toHex() + '-' + event.params.orderId.toHex());
 
-  offer.lastBlock = event.block.number;
+  if (!order) return;
 
-  offer.active = false;
-  offer.save();
+  order.lastBlock = event.block.number;
+
+  order.bidder = event.params.bidder;
+  order.bidAmount = event.params.bidAmount;
+
+  order.save();
+
+  let bid = new Bid(event.transaction.hash.toHex() + '-' + event.logIndex.toString());
+  bid.createdAt = event.block.timestamp;
+  bid.order = order.id;
+
+  bid.bidder = event.params.bidder;
+  bid.bidAmount = event.params.bidAmount;
+
+  bid.save();
 }
 
-export function handleOfferAccepted(event: OfferAccepted): void {
-  let offer = new Offer(event.address.toHex() + '_' + event.params.offerId.toHex());
-  if (!offer) return;
+export function handleOrderFilled(event: OrderFilled): void {
+  let order = new Order(event.address.toHex() + '-' + event.params.orderId.toHex());
 
-  offer.lastBlock = event.block.number;
+  if (!order) return;
 
-  offer.active = false;
-  offer.buyer = event.params.buyer;
-  offer.save();
+  order.lastBlock = event.block.number;
+
+  order.open = false;
+  order.taker = event.params.taker;
+  order.paidAmount = event.params.paidAmount;
+
+  order.save();
+}
+
+export function handleOrderCancelled(event: OrderCancelled): void {
+  let order = new Order(event.address.toHex() + '-' + event.params.orderId.toHex());
+
+  if (!order) return;
+
+  order.lastBlock = event.block.number;
+
+  order.open = false;
+
+  order.save();
 }
