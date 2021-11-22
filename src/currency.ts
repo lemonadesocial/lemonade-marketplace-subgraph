6b58bc42aacdf5f40daf1e6d13629727696c0201
Chris Lahaye <mail@chrislahaye.com>
2021-11-22 14:50:42 +0100

    refactor(currency): replace erc20 by ierc20metadata contract
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/currency.ts
+++ src/currency.ts
@@ -1,16 +1,16 @@
 import { Address } from '@graphprotocol/graph-ts';
 
 import { Currency } from '../generated/schema'
-import { ERC20 } from '../generated/LemonadeMarketplace/ERC20'
+import { IERC20Metadata } from '../generated/LemonadeMarketplace/IERC20Metadata'
 
 export function fetchCurrency(address: Address): Currency {
   let currencyID = address.toHex();
   let currency = Currency.load(currencyID)
 
   if (!currency) {
-    let erc20 = ERC20.bind(address);
-    let name = erc20.try_name();
-    let symbol = erc20.try_symbol();
+    let erc20Metadata = IERC20Metadata.bind(address);
+    let name = erc20Metadata.try_name();
+    let symbol = erc20Metadata.try_symbol();
 
     currency = new Currency(currencyID);
 
