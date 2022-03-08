5cfeca0c1f2bbeb8668e1df96f9cce62051301ee
Chris Lahaye <mail@chrislahaye.com>
2022-03-08 15:02:56 +0100

    feat: allow manifest preparation without graft
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- scripts/manifest.sh
+++ scripts/manifest.sh
@@ -2,5 +2,9 @@
 
 ln -fs subgraph.$1.yaml subgraph.yaml
 
-BLOCK=`yq -r '.graft.block-1' subgraph.yaml`
-yq -y ".graft.block=$BLOCK" subgraph.yaml > subgraph.backup.yaml
+BLOCK=`yq -r '.graft.block-1' subgraph.yaml 2> /dev/null`
+if [ "$?" -eq "0" ]; then
+  yq -y ".graft.block=$BLOCK" subgraph.yaml > subgraph.backup.yaml
+else
+  cp subgraph.yaml subgraph.backup.yaml
+fi
