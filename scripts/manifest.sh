6892149c6a8b134177897772d2f35ccd35cc0396
Chris Lahaye <mail@chrislahaye.com>
2022-04-13 18:12:27 +0200

    build: remove backup manifest
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- scripts/manifest.sh
+++ scripts/manifest.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
 ln -fs subgraph.$1.yaml subgraph.yaml
-
-BLOCK=`yq -r '.graft.block-1' subgraph.yaml 2> /dev/null`
-if [ "$?" -eq "0" ]; then
-  yq -y ".graft.block=$BLOCK" subgraph.yaml > subgraph.backup.yaml
-else
-  cp subgraph.yaml subgraph.backup.yaml
-fi
