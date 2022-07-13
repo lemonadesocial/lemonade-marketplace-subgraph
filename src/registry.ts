e39d9627749f4132d9f70fb032e10c4124d4aed1
Chris Lahaye <mail@chrislahaye.com>
2022-07-13 10:08:49 +0200

    feat: use unique chain specific interface id
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/registry.ts
+++ src/registry.ts
@@ -3,7 +3,11 @@ import { Address, Bytes } from '@graphprotocol/graph-ts';
 import { IERC165 } from '../generated/IERC721/IERC165'
 import { Registry } from '../generated/schema'
 
-let IERC721InterfaceId = Bytes.fromHexString('80ac58cd');
+function interfaceId(address: string): Bytes {
+  if (address.startsWith('0x17c4e6453cc49aaaaeaca894e6d9683e')) return Bytes.fromHexString('58800161');
+
+  return Bytes.fromHexString('80ac58cd');
+}
 
 export function fetchRegistry(address: Address): Registry {
   let registryID = address.toHex();
@@ -11,7 +15,7 @@ export function fetchRegistry(address: Address): Registry {
 
   if (!registry) {
     let erc165 = IERC165.bind(address);
-    let supportsERC721 = erc165.try_supportsInterface(IERC721InterfaceId as Bytes);
+    let supportsERC721 = erc165.try_supportsInterface(interfaceId(registryID));
 
     registry = new Registry(registryID);
     registry.isERC721 = supportsERC721.reverted ? false : supportsERC721.value;
