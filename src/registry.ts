4d4fe8da76834e55144d62a213815a0e540d4829
Chris Lahaye <mail@chrislahaye.com>
2023-05-10 10:20:38 +0200

    feat(registry): temporarily hack around rpc error on astar
    Signed-off-by: Chris Lahaye <mail@chrislahaye.com>

--- src/registry.ts
+++ src/registry.ts
@@ -1,22 +1,14 @@
-import { Address, Bytes } from '@graphprotocol/graph-ts';
+import { Address } from '@graphprotocol/graph-ts';
 
-import { IERC165 } from '../generated/IERC721/IERC165'
 import { Registry } from '../generated/schema'
-
-let IERC721InterfaceId = Bytes.fromHexString('80ac58cd');
 
 export function fetchRegistry(address: Address): Registry {
   let registryID = address.toHex();
   let registry = Registry.load(registryID)
 
-  if (!registry) {
-    let erc165 = IERC165.bind(address);
-    let supportsERC721 = erc165.try_supportsInterface(IERC721InterfaceId as Bytes);
-
-    registry = new Registry(registryID);
-    registry.isERC721 = supportsERC721.reverted ? false : supportsERC721.value;
-    registry.save();
-  }
+  registry = new Registry(registryID);
+  registry.isERC721 = true;
+  registry.save();
 
   return registry;
 }
